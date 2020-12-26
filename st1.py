ba8e74b7737e4034131c546c0f5bf205a753b518
Mohammed Muzzammil <36696171+mohammed-muzzammil@users.noreply.github.com>
2020-12-26 17:18:30 +0530

    Update st1.py

--- st1.py
+++ st1.py
@@ -1,7 +1,3 @@
   
 import streamlit as st
-import os
-ON_HEROKU = os.environ.get('ON_HEROKU')
-if ON_HEROKU:
-  port = int(os.environ.get("PORT", 5000))
-  st.title("Hello guys")
+st.title("Hello guys")
